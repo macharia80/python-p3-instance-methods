484ded339ebc2779f6aeac1e3471ab82cd642b2f
macharia <mrmachariaorsam@gmail.com>
2025-05-13 09:02:46 +0300

    last commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def sit(self):
+        print("The dog is sitting.")
     # Class body goes here
 
     #Instance method definition
--- lib/person.py
+++ lib/person.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 class Person:
+    def talk(self):
+        print("The person is walking.")
     # Class body goes here
 
     #Instance method definition
